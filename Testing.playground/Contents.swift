fc6d79f404e16fb5f25fa1d503e97f335e2c4670
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-09-25 12:50:14 -0600

    using math in swift. Ints / Doubles are used.

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -6,12 +6,24 @@ var myName :String
 var thisName :String = "World"
 let otherName = "meow"
 
+print(thisName.count)
+print(thisName.characters.count)
+
 let newMew = otherName.replacingOccurrences(of: "meow", with: "mew", options: .literal)
 
 print("Hello, \(thisName)")
+
 
 var word = "Count"
 let numberOfChars = word.ccharacters.count
 let numberofDistinctChars = Set(wordcharacters).count
 let occurenciesOfCount = word.characters.filter { $0 == "Count" }.count
 print(occurenciesOfCount)
+
+
+
+var age = 2833
+var speed = 652.298382
+var rate = age/Int (speed)
+var ratez = Double (age)/speed)
+print(rate)
