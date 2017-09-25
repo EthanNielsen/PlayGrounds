49eac62e44d1da2b6812c934e5cbc1973acb74f8
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-09-25 13:16:29 -0600

    Eyyy I  fixed my code, it had a ton of errors but fixed them.

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -5,13 +5,12 @@ import Cocoa
 var age = 2833
 var speed = 652.298382
 var rate = age/Int (speed)
-var ratez = Double (age)/speed)
+
 
 var myName :String
 var thisName :String = "World"
 let otherName = "meow"
 
-print(thisName.count)
 print(thisName.characters.count)
 
 let newMew = otherName.replacingOccurrences(of: "meow", with: "mew", options: .literal)
@@ -19,15 +18,4 @@ let newMew = otherName.replacingOccurrences(of: "meow", with: "mew", options: .l
 print("Hello, \(thisName)")
 
 
-var word = "Count"
-let numberOfChars = word.ccharacters.count
-let numberofDistinctChars = Set(wordcharacters).count
-let occurenciesOfCount = word.characters.filter { $0 == "Count" }.count
-print(occurenciesOfCount)
 
-
-
-var age = 2833
-var speed = 652.298382
-var rate = age/Int (speed)
-var ratez = Double (age)/speed)
