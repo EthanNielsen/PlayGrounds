5f1396193894448d06eb344d5a1b54b0b2458e2b
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-09-25 12:39:31 -0600

    Created a String interpolation with world. Trying to create code to count characters

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -3,8 +3,15 @@
 import Cocoa
 
 var myName :String
-var thisName :String = "Worlds"
+var thisName :String = "World"
 let otherName = "meow"
 
 let newMew = otherName.replacingOccurrences(of: "meow", with: "mew", options: .literal)
 
+print("Hello, \(thisName)")
+
+var word = "Count"
+let numberOfChars = word.ccharacters.count
+let numberofDistinctChars = Set(wordcharacters).count
+let occurenciesOfCount = word.characters.filter { $0 == "Count" }.count
+print(occurenciesOfCount)
