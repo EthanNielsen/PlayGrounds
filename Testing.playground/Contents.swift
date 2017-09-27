21101f58104982c3b207bd99d792a6674bcfb242
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-09-27 11:47:27 -0600

    back un swift starting this up and ccompying part of the teachers code.

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -2,20 +2,52 @@
 
 import Cocoa
 
-var age = 2833
-var speed = 652.298382
-var rate = age/Int (speed)
+public func isReallySimplee() -> Void
+{
+    print("It will be warm this weeken!!!! Yay for 70+ grees")
+}
 
 
-var myName :String
-var thisName :String = "World"
-let otherName = "meow"
 
-let newMew = otherName.replacingOccurrences(of: "meow", with: "mew", options: .literal)
 
-print(thisName.characters.count)
 
-print("Hello, \(thisName)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var age = 2833
+//var speed = 652.298382
+//var rate = age/Int (speed)
+//
+//
+//var myName :String
+//var thisName :String = "World"
+//let otherName = "meow"
+//
+//let newMew = otherName.replacingOccurrences(of: "meow", with: "mew", options: .literal)
+//
+//print(thisName.characters.count)
+//
+//print("Hello, \(thisName)")
 
 
 
