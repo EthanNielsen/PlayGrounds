464a15446be92e453df8989d6dfa8a6b9f4d0ea6
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-10-04 12:38:15 -0600

    Tryingo create multiple UIViews. "Trying" :L

--- GUIPlayground.playground/Contents.swift
+++ GUIPlayground.playground/Contents.swift
@@ -2,4 +2,18 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var topic = "Working with GUI :D"
+
+var myFirstButton = UIButton(frame: CGRect(x: 30, y: 60, width: 300, height: 120))
+myFirstButton.setTitle("Words", for: .normal)
+myFirstButton.backgroundColor = .green
+
+let rect = CGRect(x: 10, y: 10, width: 100, height: 100)
+let myView = UIView(frame: rect)
+
+var frame: CGRect { get myView }
+// class var layerClass: AnyClass { int }
+
+var isHidden: Bool { myView }
+
+var alpha:CGFloat { get set }
