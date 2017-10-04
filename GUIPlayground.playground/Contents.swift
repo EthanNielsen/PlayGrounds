fa5e7ca0aae024772744c62c0d829a85797d6707
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-10-04 12:56:27 -0600

    created a bbetter "myView''s and now working on buttons

--- GUIPlayground.playground/Contents.swift
+++ GUIPlayground.playground/Contents.swift
@@ -8,12 +8,18 @@ var myFirstButton = UIButton(frame: CGRect(x: 30, y: 60, width: 300, height: 120
 myFirstButton.setTitle("Words", for: .normal)
 myFirstButton.backgroundColor = .green
 
+
+
+
+
 let rect = CGRect(x: 10, y: 10, width: 100, height: 100)
 let myView = UIView(frame: rect)
 
-var frame: CGRect { get myView }
-// class var layerClass: AnyClass { int }
+myView.backgroundColor = .blue
+myView.backgroundColor = .yellow
+myView.backgroundColor = .green
 
-var isHidden: Bool { myView }
 
-var alpha:CGFloat { get set }
+var myFunnyButton = UIButton(frame: CGRect(x: 7, y: 7, width: 100, height: 50))
+myFunnyButton.setTitle("DANK MEMES", for: .normal)
+myFirst
